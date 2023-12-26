70310d0ebebb067a1ed649df2be8cd774b496750
Leandro Luiz Pereira <leandro.pereira.moon@hotmail.com>
2023-12-26 13:31:41 -0300

    create

--- static/function.js
+++ static/function.js
@@ -3,4 +3,6 @@ console.log('Hello , world !!');
 alert('Hello , world');
 
 
-document.getElementById(js).innerHTML="JS";
+//document.getElementById(js).innerHTML="JS";
+//document.getElementById(js).textContent="JS";
+//document.getElementById('js').innerText="JS";
